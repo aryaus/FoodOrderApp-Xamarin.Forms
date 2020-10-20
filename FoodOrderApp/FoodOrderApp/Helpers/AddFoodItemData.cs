4a1119cd8f2766b1a397553dfc56313a05cfd41c
Arya <mohammad.ghiri@bayoo.net>
2020-10-20 17:26:57 +0200

    added Categories and Food Items

---
+++ FoodOrderApp/FoodOrderApp/Helpers/AddFoodItemData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodOrderApp.Models;
+
+namespace FoodOrderApp.Helpers
+{
+    public class AddFoodItemData
+    {
+        public List<FoodItem> FoodItems { get; set; }
+
+        public AddFoodItemData()
+        {
+            FoodItems = new List<FoodItem>()
+            {
+                new FoodItem
+                {
+                    ProductId = 1,
+                    CategoryID = 1,
+                    ImageUrl = "MainBurger.png",
+                    Name = "Burger and Pizza Hub 1",
+                    Description = "Burger´- Pizza - Breakfast",
+                    Rating = "4.8",
+                    RatingDetail = "(121 Ratings)",
+                    HomeSelected = "CompleteHeart",
+                    Price = 45
+                },
+                 new FoodItem
+                {
+                    ProductId = 2,
+                    CategoryID = 1,
+                    ImageUrl = "MainBurger.png",
+                    Name = "Burger and Pizza Hub 1",
+                    Description = "Burger´- Pizza - Breakfast",
+                    Rating = "4.8",
+                    RatingDetail = "(121 Ratings)",
+                    HomeSelected = "CompleteHeart",
+                    Price = 45
+                },
+            };
+
+        }
+    }
+}
